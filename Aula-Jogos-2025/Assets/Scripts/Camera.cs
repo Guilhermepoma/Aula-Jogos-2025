be68ee4cbb4fea9af35ef29a58a05c684d41d5a6
Guilherme_Poma_Cercena <guilhermepomacercena@gmail.com>
2025-10-28 15:33:19 -0300

    menu e camera

--- Aula-Jogos-2025/Assets/Scripts/Camera.cs
+++ Aula-Jogos-2025/Assets/Scripts/Camera.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
 
-public class CameraFollow2D : MonoBehaviour
+public class Camera : MonoBehaviour
 {
-    public Transform target;         // O player
-    public float smoothSpeed = 5f;   // Velocidade da câmera
-    public Vector3 offset = new Vector3(0f, 0f, -10f); // Mantém a câmera atrás da cena
+    public Transform target; // Player
+    public float smoothSpeed = 5f;
+    public Vector3 offset = new Vector3(0f, 0f, -10f);
+
+    [Header("Limites da câmera")]
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
 
     void LateUpdate()
     {
         if (target == null) return;
 
-        // Posição desejada (posição do player + offset)
+        // Posição desejada
         Vector3 desiredPosition = new Vector3(target.position.x + offset.x, target.position.y + offset.y, offset.z);
+
+        // Aplicando limites
+        desiredPosition.x = Mathf.Clamp(desiredPosition.x, minX, maxX);
+        desiredPosition.y = Mathf.Clamp(desiredPosition.y, minY, maxY);
 
         // Movimento suave
         transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
--- Aula-Jogos-2025/Assets/Scripts/Menu.cs
+++ Aula-Jogos-2025/Assets/Scripts/Menu.cs
@@ -4,19 +4,36 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ActiveConfig(GameObject go)
+    {
+        go.SetActive(true);
+    }
+
+    public void DisableConfig(GameObject go)
+    {
+        go.SetActive(false);
+    }
+
+    public void ActivePause(GameObject go)
+    {
+        Time.timeScale = 0;
+        go.SetActive(true);
+    }
+
+    public void DisablePause(GameObject go)
+    {
+        Time.timeScale = 1;
+        go.SetActive(false);
+    }
+
+    public void MenuPause()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
     }
 }
